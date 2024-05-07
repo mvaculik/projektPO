d48296b1691a6c3aa328c57e58575394f3e2db2f
mvaculik <73494801+mvaculik@users.noreply.github.com>
2024-05-07 09:13:47 +0200

    Create Level.cs

---
+++ Level.cs
@@ -0,0 +1,11 @@
+using Godot;
+using System;
+
+public class Level : Node2D
+{
+    public override void _Ready()
+    {
+        GD.Print("Level Started");
+        // Additional level setup can be done here
+    }
+}
